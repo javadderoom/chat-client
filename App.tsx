58c38c3544d301785defc7be5e0e24ee46389d34
Javad <deroom24@gmail.com>
2026-02-16 19:30:14 +0330

    run

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import './index.css';
 
 const DEFAULT_SETTINGS: UserSettings = {
   username: `User-${Math.floor(Math.random() * 1000)}`,
-  serverUrl: import.meta.env.VITE_SERVER_URL || 'http://localhost:3000',
+  serverUrl: import.meta.env.VITE_SERVER_URL || 'https://deroom.runflare.run',
   isDemoMode: false,
 };
 
